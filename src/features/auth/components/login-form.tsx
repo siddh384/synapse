2874b145fa377f1c6d63a43fb13dc12163d71eb2
siddh384 <siddhvyas384@gmail.com>
2026-03-08 19:03:06 +0530

    basic theme updated

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -82,6 +82,12 @@ export function LoginForm() {
                     type="button"
                     disabled={isPending}
                   >
+                    <Image
+                      alt="github"
+                      src="/logos/github.svg"
+                      width={20}
+                      height={20}
+                    />
                     Continue with Github
                   </Button>
                   <Button
@@ -90,6 +96,12 @@ export function LoginForm() {
                     type="button"
                     disabled={isPending}
                   >
+                    <Image
+                      alt="google"
+                      src="/logos/google.svg"
+                      width={20}
+                      height={20}
+                    />
                     Continue with Google
                   </Button>
                 </div>
