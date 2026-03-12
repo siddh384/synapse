4b54af5e2b11a91183d2dcaca4febaedd92a85d1
siddh384 <siddhvyas384@gmail.com>
2026-03-12 11:46:13 +0530

    polar added for payment integration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,5 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth";
+import { polarClient } from "./polar";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
@@ -9,4 +11,23 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "a3047037-2822-4461-aae8-e6976960a8b9",
+              slug: "pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 });
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,15 @@
 import { inngest } from "@/inngest/client";
 import { google } from "@ai-sdk/google";
 import { generateText } from "ai";
-import { createTRPCRouter, protectedProcedure } from "../init";
+import {
+  createTRPCRouter,
+  premiumProcedure,
+  protectedProcedure,
+} from "../init";
 import prisma from "@/lib/db";
 
 export const appRouter = createTRPCRouter({
-  testAi: protectedProcedure.mutation(async () => {
+  testAi: premiumProcedure.mutation(async () => {
     await inngest.send({ name: "execute/ai" });
 
     return { success: true, message: "Job queued" };
