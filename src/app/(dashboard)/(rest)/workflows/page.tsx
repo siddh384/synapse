603a3a92489b44c98e9a038991c3eef5698e966c
siddh384 <siddhvyas384@gmail.com>
2026-03-16 11:49:10 +0530

    pagination added in workflows page

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -2,16 +2,22 @@ import {
   WorkflowsContainer,
   WorkflowsList,
 } from "@/features/workflows/components/workflows";
+import { workflowsParamsLoader } from "@/features/workflows/server/params-loader";
 import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
 import { HydrateClient } from "@/trpc/server";
+import type { SearchParams } from "nuqs/server";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-const Page = async () => {
-  await requireAuth();
+type Props = {
+  searchParams: Promise<SearchParams>;
+};
 
-  prefetchWorkflows();
+const Page = async ({ searchParams }: Props) => {
+  await requireAuth();
+  const params = await workflowsParamsLoader(searchParams);
+  prefetchWorkflows(params);
 
   return (
     <WorkflowsContainer>
